51f2514b61ceb12a93a53bfbc4d7c20ba7e75dd3
oushi <aisha.mahmoud72007@gmail.com>
2024-07-17 21:11:59 +0200

    Writing the game logic

--- script.js
+++ script.js
@@ -24,5 +24,77 @@ fetch("./data/card.json")
     temporaryValue = cards[currentIndex];
     cards[currentIndex] = cards[randomIndex];
     cards[randomIndex] = temporaryValue;
+    }
+  }
+
+    function generateCards(){
+      for(let card of cards){
+      const cardElement = document.createElement("div");
+      cardElement.classList.add("card");
+      cardElement.setAttribute("data-name", card.name);
+      cardElement.innerHTML = `
+        <div class="front>
+        <img class="front-image" src=${card.image}/>
+        </div>
+        <div class= "back"></div>
+        `;
+        gridContainer.appendChild(cardElement);
+        cardElement.addEventListener("click", flipCard);
       }
     }
+
+    function flipCard(){
+      if(lockBoard)return;
+      if(this === firstCard)return;
+
+      this.classList.add("flipped");
+
+      if(!firstCard){
+        firstCard = this;
+      }
+
+      secondCard = this;
+      score++;
+      document.querySelector(".score").textContent = score;
+      lockBoard = true;
+
+      checkForMatch();
+    }
+
+    function checkForMatch(){
+      let isMatch = firstCard.dataset.name === secondCard.dataset.name;
+
+      isMatch ? disableCards() : unflipCards();
+    }
+
+    function disableCards(){
+      firstCard.removeEventListener("click", flipCard);
+      secondCard.removeEventListener("click", flipCard);
+
+      resetBoard();
+    }
+
+    function unflipCards(){
+      setTimeout(() => {
+        firstCard.classList.remove("flipped");
+        secondCard.classList.remove("flipped");
+        resetBoard();
+      }, 1000);
+    }
+
+    function resetBoard(){
+      firstCard = null;
+      secondCard = null;
+      lockBoard = false;
+    }
+
+    function restart() {
+      resetBoard();
+      shuffelCards();
+      score = 0;
+      document.querySelector(".score").textContent = score;
+      gridContainer.innerHTML = "";
+      generateCards();
+    }
+
+    
